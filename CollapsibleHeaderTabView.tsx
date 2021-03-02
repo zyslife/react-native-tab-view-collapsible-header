c84a1c8bf6c1274c3372f98e37d1fd32cb6d7ef4
zou yu <zouyu@benewtech.cn>
2021-03-02 20:26:27 +0800

    add makeRoomInRefreshing

--- CollapsibleHeaderTabView.tsx
+++ CollapsibleHeaderTabView.tsx
@@ -1,3 +1,3 @@
-import { createHeaderTabsComponent } from './index'
+import createHeaderTabsComponent from './createHeaderTabsComponent'
 import { TabView } from 'react-native-tab-view'
 export default createHeaderTabsComponent(TabView)
